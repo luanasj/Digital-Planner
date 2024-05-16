6ba4840565b2bb9fb64e7ea8ebe7441c8cc04684
luanasj <luanasj@outlook.com.br>
2024-05-16 19:44:17 -0300

    criacao de todas as tabelas e foreign keys na db

--- appointmente_book_script.sql
+++ appointmente_book_script.sql
@@ -7,14 +7,51 @@ create table goalsAndTasks(
     s_description_goalsAndTasks varchar(255) not null,
     i_type_goalsAndTasks int not null,
     b_accomplished_goalsAndTasks boolean not null,
-    d_creation_goalAndTasks date not null
+    d_creation_goalsAndTasks date not null,
+    i_dayweek_goalsAndTasks int not null
 
 );
 
 create table goalOrTask (
-    i_goalOrTask_goalOrTask int primary key auto_increment,
+    i_goalOrTask_goalOrTask int primary key,
     s_dsc_goalOrTask varchar(50) not null
 );
 
 alter table goalsAndTasks add constraint fk_type_goalsAndTasks foreign key 
 (i_type_goalsAndTasks) references goalOrTask (i_goalOrTask_goalOrTask);
+
+create table dayweek (
+    i_dayweek_dayweek int primary key,
+    s_dayname_dayweek varchar(50) not null
+);
+
+alter table goalsAndTasks add constraint fk_dayweek_goalsAndTasks foreign key
+(i_dayweek_goalsAndTasks) references dayweek (i_dayweek_dayweek);
+
+create table schedule(
+    s_activity_schedule varchar(50),
+    i_period_schedule int not null,
+    i_dayweek_schedule int not null
+);
+
+create table schedulePeriods (
+    i_period_schedulePeriods primary key,
+    s_periodDsc_schedulePeriods varchar(50) not null
+);
+
+alter table schedule add constraint fk_period_schedule foreign key
+(i_period_schedule) references schedulePeriods (i_period_schedulePeriods);
+
+alter table schedule add constraint fk_dayweek_schedule foreign key
+(i_dayweek_schedule) references dayweek (i_dayweek_dayweek);
+
+create table contacts (
+    i_id_contacts int primary key auto_increment,
+    s_nome_contacts varchar(255),
+    s_vinculo_contacts varchar(255),
+    s_disciplina_contacts varchar(255),
+    s_email_contacts varchar(255),
+    s_telefone_contacts varchar(255),
+    s_site_contacts varchar(255),
+    s_instituicao_contacts varchar(255)
+);
