a99a51d91f70a890230b7e1cbd910625cd8ba6f3
luanasj <luanasj@outlook.com.br>
2024-05-25 18:04:40 -0300

    adicao de tratamento de erros nos controllers api-v2 e criacao variaveis de ambiente

--- api-v2/dbconnection/db.js
+++ api-v2/dbconnection/db.js
@@ -1,8 +1,11 @@
+const dotenv = require('dotenv').config()
+const {MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE,MYSQL_PORT} = process.env
+
 const conectar = async ()=>{
     if(global.connection && global.connection.state != 'disconected')
         return  global.connection
     const mysql = require('mysql2/promise')
-    const con=mysql.createPool("mysql://root:LADYbieber63@localhost:3306/appointmentbook")
+    const con=mysql.createPool(`mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}`)
     console.log('Conectou ao banco')
     global.connection = con
     return con
--- api-v2/resources/appointmentbook.controllers.js
+++ api-v2/resources/appointmentbook.controllers.js
@@ -1,85 +1,173 @@
-const db = require('../db')
+const db = require('../dbconnection/db')
 
 /*Get*/
 
 const getWeekGoalsAndTasks = async (datas)=>{
-    const con = await db.conectar()
-    const {data1,data2} = datas
-    const sql = "call goalsAnsTasksSelect(?,?);"
-    const [linhas] = await con.query(sql,[data1,data2])
-    return await linhas
+    try{
+        const con = await db.conectar()
+        if(!datas){
+            throw new Error("information missing")
+        }
+        const {data1,data2} = datas
+        const sql = "call goalsAnsTasksSelect(?,?);"
+        const [linhas] = await con.query(sql,[data1,data2])
+        return await linhas
+    }catch(e){
+        const erro = `erro: ${e}`
+        return erro
+    }
+    
 }
 
 const getSchedule = async ()=>{
-    const con = await db.conectar()
-    let sql = "select * from schedule;";
-    const [linhas] = await con.query(sql)
-    return await linhas
+    try{
+        const con = await db.conectar()
+        let sql = "select * from schedule;";
+        const [linhas] = await con.query(sql)
+        return await linhas
+    }catch(e){
+        const erro = `erro: ${e}`
+        return erro
+    }
+    
 }
 
 const getContacts = async ()=>{
-    const con = await db.conectar()
-    let sql = "select * from contacts;";
-    const [linhas] = await con.query(sql)
-    return await linhas
+    try{
+        const con = await db.conectar()
+        let sql = "select * from contacts;";
+        const [linhas] = await con.query(sql)
+        return await linhas
+    }catch(e){
+        const erro = `erro: ${e}`
+        return erro
+    }
+    
 }
 
 /*Post*/
 
 const createGoalOrTask = async (task) =>{
-    const con = await db.conectar()
-    const {dsc,typenum,acc,dt} = task
-    let sql = "call goalsAndTasksInsert(?,?,?,?);"
-    return await con.query(sql, [dsc,typenum,acc,dt]);
+    try{
+        const con = await db.conectar()
+        if(!task){
+            throw new Error("information missing")
+        }
+        const {dsc,typenum,acc,dt} = task
+        let sql = "call goalsAndTasksInsert(?,?,?,?);"
+        return await con.query(sql, [dsc,typenum,acc,dt]);
+    }catch(e){
+        const erro = `erro: ${e}`
+        return erro
+    }
+
+    
 
 }
 
 const createContact = async (contact) =>{
-    const con = await db.conectar()
-    const {nomectt,vinculo,disciplina,email,telefone,sitectt,instituicao} = contact
-    let sql = "call insertContacts(?,?,?,?,?,?,?);"
-    return await con.query(sql, [nomectt,vinculo,disciplina,email,telefone,sitectt,instituicao]);
-
+    try {
+        const con = await db.conectar()
+        if(!contact){
+            throw new Error("information missing")
+        }
+        const {nomectt,vinculo,disciplina,email,telefone,sitectt,instituicao} = contact
+        let sql = "call insertContacts(?,?,?,?,?,?,?);"
+        return await con.query(sql, [nomectt,vinculo,disciplina,email,telefone,sitectt,instituicao]);
+    } catch (e) {
+        const erro = `erro: ${e}`
+        return erro
+    }
+    
 }
 
 /*Patch*/
 
 const updateContacts = async (id,info)=>{
-    const con = await db.conectar()
-    const {nomectt,vinculo,disciplina,email,telefone,sitectt,instituicao} = info
-    const sql = "call contactsUpdate(?,?,?,?,?,?,?,?);"  
-    return await con.query(sql,[id,nomectt,vinculo,disciplina,email,telefone,sitectt,instituicao])
+    try {
+        const con = await db.conectar()
+        if(!info){
+            throw new Error("information missing")
+        }
+        if(!id){
+            throw new Error("id missing")
+        }
+        const {nomectt,vinculo,disciplina,email,telefone,sitectt,instituicao} = info
+        const sql = "call contactsUpdate(?,?,?,?,?,?,?,?);"  
+        return await con.query(sql,[id,nomectt,vinculo,disciplina,email,telefone,sitectt,instituicao])
+    } catch (e) {
+        
+    }
+
+    
 }
 
 const updateGoalsAndTasks = async (id,info) =>{
-    const con = await db.conectar()
-    const {campo,entrada} = info //campo must be "description" or "acc" && entrada must be a string
-    const sql = "call goalsAndTasksUpdate(?,?,?);"
-    return await con.query(sql,[campo,entrada,id])
+    try {
+        const con = await db.conectar()
+        if(!info){
+            throw new Error("information missing")
+        }
+        if(!id){
+            throw new Error("id missing")
+        }
+        const {campo,entrada} = info //campo must be "description" or "acc" && entrada must be a string
+        const sql = "call goalsAndTasksUpdate(?,?,?);"
+        return await con.query(sql,[campo,entrada,id])
+    } catch (e) {
+        const erro = `erro: ${e}`
+        return erro
+    }
+    
 
 }
 
 const updateSchedule = async (info)=>{
-    const con = await db.conectar()
-    const {newActivity,dayweek,scperiod} = info
-    const sql = "call scheduleUpdate(?,?,?);"
-    return await con.query(sql,[newActivity,dayweek,scperiod])
+    try {
+        const con = await db.conectar()
+        if(!info){
+            throw new Error("information missing")
+        }
+        const {newActivity,dayweek,scperiod} = info
+        const sql = "call scheduleUpdate(?,?,?);"
+        return await con.query(sql,[newActivity,dayweek,scperiod])
+    } catch (e) {
+        const erro = `erro: ${e}`
+        return erro
+    }
+   
 }
 
 /*Delete*/
 
 const deleteGoalsAndTasks = async (id)=>{
-    const con = await db.conectar()
-    const sql = "call goalsAndTasksDelete(?)"
-    return await con.query(sql,id)
+    try {
+        if(!id){
+            throw new Error("id missing")
+        }
+        const con = await db.conectar()
+        const sql = "call goalsAndTasksDelete(?)"
+        return await con.query(sql,id)
+    } catch (e) {
+        const erro = `erro: ${e}`
+        return erro   
+    }
 
 }
 
 
 const deleteContact = async (id)=>{
-    const con = await db.conectar()
-    const sql = "call contactsDelete(?)"
-    return await con.query(sql,id)
+    try {
+        if(!id){
+            throw new Error("id missing")
+        }
+        const con = await db.conectar()
+        const sql = "call contactsDelete(?)"
+        return await con.query(sql,id)
+    } catch (e) {
+        const erro = `erro: ${e}`
+        return erro   
+    }
 }
 
 
